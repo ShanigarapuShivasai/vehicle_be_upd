238e455539d8021dda86cdde00136ae65a0325ba
Shivasai <153177624+ShanigarapuShivasai@users.noreply.github.com>
2024-08-26 13:27:26 +0530

    Add files via upload

---
+++ src/main/java/com/example/Vehicle/model/Brand.java
@@ -0,0 +1,27 @@
+package com.example.Vehicle.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import com.example.Vehicle.model.Model;
+
+import java.util.List;
+
+
+@Entity
+@Data
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long brandId; // Manual ID assignment
+
+    private String brandName;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    @OneToMany(mappedBy = "brand")
+    private List<Model> models;
+    private String imageUrl;
+}
